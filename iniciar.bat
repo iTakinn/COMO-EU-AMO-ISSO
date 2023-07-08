90da715c0dd8ed879c74906d2a4a6d04fa6d3108
iTakinn <137518812+iTakinn@users.noreply.github.com>
2023-07-07 21:59:11 -0300

    bungee,lobby e survival :+1:

---
+++ iniciar.bat
@@ -0,0 +1,12 @@
+
+cd bungee
+start "bungee" java -Xmx256M -jar bungeecord.jar
+cd ..
+
+cd Lobby
+start "lobby" java -Xmx2G -jar purpur.jar
+cd ..
+
+cd survival
+start "survival" java -Xmx6G -jar purpur-1.19.3-1933.jar
+PAUSE
